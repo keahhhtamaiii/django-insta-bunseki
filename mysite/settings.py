64fc102a4bfc6b53e8569767fc9466d0d4c9d57c
keahhhtamaiii <keahhhtamaiii@gmail.com>
2024-10-02 00:10:09 +0900

    Update settings.py with new configurations

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'elrb5@^f&uxdj*9iyq)@&^z_)&08(98@e7p!s5g(qcq2^jwt42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['insta-bunseki-80c5a282e88d.herokuapp.com']
 
 
 # Application definition
