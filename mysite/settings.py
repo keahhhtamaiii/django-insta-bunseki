89d5bdebef9ce052124962b69453121ee7bc26b0
keahhhtamaiii <keahhhtamaiii@gmail.com>
2024-10-02 12:00:34 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'elrb5@^f&uxdj*9iyq)@&^z_)&08(98@e7p!s5g(qcq2^jwt42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
