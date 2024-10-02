59e5f47bf4366d8595e4f8a39c6253d6eba39392
keahhhtamaiii <keahhhtamaiii@gmail.com>
2024-10-02 14:41:15 +0900

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,15 +84,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-#  DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / 'db.sqlite3',
-#      }
-#  }
 
 
 # Password validation
